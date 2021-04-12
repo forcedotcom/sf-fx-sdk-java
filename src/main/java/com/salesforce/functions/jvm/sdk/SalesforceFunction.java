1badf2379d2d0c899f5ca31c078bf2509a052022
Manuel Fuchs <manuel.fuchs@salesforce.com>
2021-04-12 09:29:17 +0200

    Allow SalesforceFunction to throw Exception

--- src/main/java/com/salesforce/functions/jvm/sdk/SalesforceFunction.java
+++ src/main/java/com/salesforce/functions/jvm/sdk/SalesforceFunction.java
@@ -6,16 +6,24 @@
  */
 package com.salesforce.functions.jvm.sdk;
 
-import java.util.function.BiFunction;
-
 /**
  * Main interface for Salesforce Functions written in Java.
  *
  * @param <T> The type of the {@link InvocationEvent} payload this function can handle.
  * @param <R> The type of the response of this function.
+ * @see java.util.function.BiFunction
  */
 @SuppressWarnings("unused")
-public interface SalesforceFunction<T, R> extends BiFunction<InvocationEvent<T>, Context, R> {
-  @Override
-  R apply(InvocationEvent<T> tInvocationEvent, Context context);
+@FunctionalInterface
+public interface SalesforceFunction<T, R> {
+
+  /**
+   * Applies the function to the given arguments.
+   *
+   * @param event The invocation event for this function application.
+   * @param context The context for this function application.
+   * @return The result of this function application.
+   * @throws Exception If an unrecoverable exception occurred during function application.
+   */
+  R apply(InvocationEvent<T> event, Context context) throws Exception;
 }
