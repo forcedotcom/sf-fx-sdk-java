5783a6ffa88a5a5522e411f0ad353b8522e69cdd
Manuel Fuchs <manuel.fuchs@salesforce.com>
2022-07-29 17:26:31 +0200

    Add support for binary fields (#84)

--- src/main/java/com/salesforce/functions/jvm/sdk/data/builder/RecordBuilder.java
+++ src/main/java/com/salesforce/functions/jvm/sdk/data/builder/RecordBuilder.java
@@ -12,6 +12,7 @@ import com.salesforce.functions.jvm.sdk.data.RecordAccessor;
 import com.salesforce.functions.jvm.sdk.data.ReferenceId;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -175,4 +176,15 @@ public interface RecordBuilder extends RecordAccessor {
   @Nonnull
   @SuppressWarnings("unused")
   RecordBuilder withField(String name, @Nullable ReferenceId value);
+
+  /**
+   * Sets the value of a field.
+   *
+   * @param name The name of the field.
+   * @param value The value to set.
+   * @return This {@link RecordBuilder} instance to allow method chaining.
+   */
+  @Nonnull
+  @SuppressWarnings("unused")
+  RecordBuilder withField(String name, @Nullable ByteBuffer value);
 }
