070e299c7c9fb92edc6e7eab074ecac153765c90
Colin Casey <casey.colin@gmail.com>
2022-11-08 12:17:56 -0400

    Migrate from Circle CI to GitHub Actions (#102)
    * feat: migrate from Circle CI to GitHub Actions
    
    [W-11838222](https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07EE000017dCK4YAM/view)

--- src/main/java/com/salesforce/functions/jvm/sdk/data/builder/package-info.java
+++ src/main/java/com/salesforce/functions/jvm/sdk/data/builder/package-info.java
@@ -4,6 +4,12 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+/**
+ * This module contains the builder interfaces that can be used to construct objects that can be
+ * operated on with the {@link com.salesforce.functions.jvm.sdk.data.DataApi} and {@link
+ * com.salesforce.functions.jvm.sdk.data.UnitOfWork}.
+ */
 @ParametersAreNonnullByDefault
 package com.salesforce.functions.jvm.sdk.data.builder;
 
--- src/main/java/com/salesforce/functions/jvm/sdk/data/package-info.java
+++ src/main/java/com/salesforce/functions/jvm/sdk/data/package-info.java
@@ -4,6 +4,11 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+/**
+ * This module contains the interfaces that support query and persist operations against the
+ * Salesforce API.
+ */
 @ParametersAreNonnullByDefault
 package com.salesforce.functions.jvm.sdk.data;
 
